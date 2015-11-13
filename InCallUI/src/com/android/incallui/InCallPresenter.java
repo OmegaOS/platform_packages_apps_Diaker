5c88e63034d11b539d671d3ab527f5b9964a8b29
Danny Baumann <dannybaumann@web.de>
2015-11-13 15:26:46 -0800

    Dialer: Add back in-call vibration features
    Fix call tracking.
    getActiveCall() can return null when ending calls locally before they
    are answered locally, so better use the presence of an active call as
    indicator of whether to do the vibration instead of using the state.
    
    Change-Id: I088cd743c9a9e6704e93f8c48fb566b3a20ca8be

--- InCallUI/src/com/android/incallui/InCallPresenter.java
+++ InCallUI/src/com/android/incallui/InCallPresenter.java
@@ -114,6 +114,7 @@ public class InCallPresenter implements CallList.Listener,
     private AudioModeProvider mAudioModeProvider;
     private StatusBarNotifier mStatusBarNotifier;
     private ExternalCallNotifier mExternalCallNotifier;
+    private InCallVibrationHandler mInCallVibrationHandler;
     private ContactInfoCache mContactInfoCache;
     private Context mContext;
     private CallList mCallList;
@@ -326,6 +327,9 @@ public class InCallPresenter implements CallList.Listener,
         mContext = context;
 
         mContactInfoCache = contactInfoCache;
+
+        mInCallVibrationHandler = new InCallVibrationHandler(context);
+        addListener(mInCallVibrationHandler);
 
         mStatusBarNotifier = statusBarNotifier;
         mExternalCallNotifier = externalCallNotifier;
@@ -1695,6 +1699,10 @@ public class InCallPresenter implements CallList.Listener,
 
             InCallCsRedialHandler.getInstance().tearDown();
 
+            if (mInCallVibrationHandler != null) {
+                removeListener(mInCallVibrationHandler);
+            }
+            mInCallVibrationHandler = null;
             if (mCallList != null) {
                 mCallList.removeListener(this);
             }
